fa1be097992dada238635eeefa9d23327ac8ca8f
SJ <sjmun0420@gmail.com>
2022-11-08 20:20:09 +0900

    스트럭쳐 다품

---
+++ data_structures/unit26.py
@@ -0,0 +1,15 @@
+#연습문제
+a = {i for i in range(1,101) if i%3==0}
+b = {i for i in range(1,101) if i%5==0}
+print(a & b)
+
+#심사문제
+a,b = map(int, input("숫자 두개: ").split())
+se1 = {i for i in range(1,a+1) if a%i==0}
+se2 = {i for i in range(1,b+1) if b%i==0}
+print(sum(se1&se2))
+
+## 핵심문법:
+# 데이터타입 set{} => 생성방법: a = set() -> 순서가 없음 => 시퀀스x
+# set안에 set넣기 x
+# 집합문법 => | : 합집합, & : 교집합, - : 차집합, ^ : 대칭차집합(=안겹치는거)
--- data_structures/unit3.py
+++ data_structures/unit3.py
@@ -1,1 +1,7 @@
-print("hi")
+# 연습문제
+print("Hello, world!")
+print("Python Programming")
+
+#심사문제
+print("Hello, world!")
+print("Hello, world!")
